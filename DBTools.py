cdf5fd049294f9f0f7b72a3bd630007b3af322dc
Craig Scarboro <57534046+cbsc228@users.noreply.github.com>
2020-04-20 12:28:26 -0400

    Add files via upload
    Connects to message broker and uploads stream to graph database

--- DBTools.py
+++ DBTools.py
@@ -1,74 +1,108 @@
 import pyorient
+import csv
+import re
 
-def getrid(client,id):
-    nodeId = client.query("SELECT FROM V WHERE id = '" + str(id) + "'")
-    if(nodeId):
-        return str(nodeId[0]._rid)
-    else:
-        return "0"
+def reset_db(client, name):
 
-def resetDB():
+   # Remove Old Database
+   if client.db_exists(name):
+    client.db_drop(name)
 
-    # database name
-    name = "agen"
-    # database login is root by default
-    login = "root"
-    # database password, set by docker param
-    password = "rootpwd"
-
-    # create client to connect to local orientdb docker container
-    client = pyorient.OrientDB("localhost", 2424)
-    session_id = client.connect(login, password)
-
-    # Remove Old Database
-    if client.db_exists(name):
-        client.db_drop(name)
-
-    # Create New Database
-    client.db_create(name,
+   # Create New Database
+   client.db_create(name,
       pyorient.DB_TYPE_GRAPH,
       pyorient.STORAGE_TYPE_PLOCAL)
 
+def importHospitals(client):
+    print("Importing hospitals into database...")
+    #open csv file of hospitals
+    filepath = "hospitals.csv"
+    file =  open(filepath)
+    readFile = csv.reader(file, delimiter = ',')
+    headers = []
+    rowCount = 0
+    for row in readFile:
+        hospitalInfo = []
+        for i in range(len(row)):
+            #gets rid of hidden garbage characters and single quotes
+            row[i] = re.sub("'", "", row[i])
+            row[i] = re.sub("[^\x20-\x7E]", "", row[i])
+            if rowCount == 0:
+                #get data headers
+                headers.append(row[i])
+            else:
+                #get hospital info
+                hospitalInfo.append(row[i])
+        if rowCount != 0:
+            #build query to insert hospital into database
+            query = "CREATE VERTEX Hospital SET "
+            for i in range(len(hospitalInfo)):
+                query += headers[i] + " = " +  "'" + hospitalInfo[i] + "'"
+                if (i != len(hospitalInfo) - 1):
+                    query += ","
+            #insert hospital into database
+            client.command(query)
+        rowCount += 1
+    
+#NEED TO FINISH---------
+#need to make it so that whenever a patient is inserted, we check if they need to be routed to a hospital
+#if so, connect them to that hospital in the database and reduce the bed count by 1
+def insertPatient(first_name, last_name, mrn, zip_code, patient_status_code, client):
+    query = "CREATE VERTEX Patient SET first_name = '" + first_name + "', last_name = '" + last_name + "', mrn = '" + mrn + "', zip_code = '" + zip_code + "', patient_status_code = '" + patient_status_code + "'"
+    client.command(query)
+    #client.close()
+    
 
-def addNode(nodeId):
-
+def loadDB():
+    print("Starting database...")
     #database name
-    dbname = "agen"
+    dbname = "covid_reporting_project"
     #database login is root by default
     login = "root"
     #database password, set by docker param
     password = "rootpwd"
 
     #create client to connect to local orientdb docker container
-    client = pyorient.OrientDB("localhost", 2424)
-    session_id = client.connect(login, password)
+    client = pyorient.OrientDB("172.31.144.183", 2424)
+    try:
+        session_id = client.connect(login, password)
+    except:
+        return False
+
+    #remove old database and create new one
+    reset_db(client,dbname)
 
     #open the database we are interested in
     client.db_open(dbname, login, password)
 
-    client.command("CREATE VERTEX SET id = '" + nodeId + "'")
-
+    client.command("CREATE CLASS Patient EXTENDS V")
+    client.command("CREATE PROPERTY Patient.first_name String")
+    client.command("CREATE PROPERTY Patient.last_name String")
+    client.command("CREATE PROPERTY Patient.mrn String")
+    client.command("CREATE PROPERTY Patient.zip_code Integer")
+    client.command("CREATE PROPERTY Patient.patient_status_code Integer")
+    
+    client.command("CREATE CLASS Hospital EXTENDS V")
+    client.command("CREATE PROPERTY Hospital.ID Integer")
+    client.command("CREATE PROPERTY Hospital.Name String")
+    client.command("CREATE PROPERTY Hospital.Address String")
+    client.command("CREATE PROPERTY Hospital.City String")
+    client.command("CREATE PROPERTY Hospital.State String")
+    client.command("CREATE PROPERTY Hospital.Zip Integer")
+    client.command("CREATE PROPERTY Hospital.Type String")
+    client.command("CREATE PROPERTY Hospital.Beds Integer")
+    client.command("CREATE PROPERTY Hospital.County String")
+    client.command("CREATE PROPERTY Hospital.Countyfips Integer")
+    client.command("CREATE PROPERTY Hospital.Country String")
+    client.command("CREATE PROPERTY Hospital.Latitude Float")
+    client.command("CREATE PROPERTY Hospital.Longitude Float")
+    client.command("CREATE PROPERTY Hospital.Naics_code Integer")
+    client.command("CREATE PROPERTY Hospital.Website String")
+    client.command("CREATE PROPERTY Hospital.Owner String")
+    client.command("CREATE PROPERTY Hospital.Trauma String")
+    client.command("CREATE PROPERTY Hospital.Helipad String")
+    
+    importHospitals(client)
+    
     client.close()
-
-
-def addEdge(nodeFrom, nodeTo):
-    # database name
-    dbname = "agen"
-    # database login is root by default
-    login = "root"
-    # database password, set by docker param
-    password = "rootpwd"
-
-    # create client to connect to local orientdb docker container
-    client = pyorient.OrientDB("localhost", 2424)
-    session_id = client.connect(login, password)
-
-    # open the database we are interested in
-    client.db_open(dbname, login, password)
-
-    nodeFromId = str(getrid(client, nodeFrom))
-    nodeToId = str(getrid(client, nodeTo))
-
-    client.command("CREATE EDGE FROM " + nodeFromId + " TO " + nodeToId)
-
-    client.close()
+    return True
--- Subscriber.py
+++ Subscriber.py
@@ -2,77 +2,182 @@
 import pika
 import sys
 import json
-from DBTools import addNode
-from DBTools import addEdge
-from DBTools import resetDB
-
-
-#Make sure orientDB version 2.2 is running, version 3.x does not work with these drivers
-
-#-d run in background login:root password:rootpwd
-#docker run -d --name orientdb -p 2424:2424 -p 2480:2480 -e ORIENTDB_ROOT_PASSWORD=rootpwd orientdb:2.2
-
-#-it run in foreground login:root password:rootpwd
-#docker run -it --name orientdb -p 2424:2424 -p 2480:2480 -e ORIENTDB_ROOT_PASSWORD=rootpwd orientdb:2.2
-
-resetDB()
-
+from DBTools import loadDB, insertPatient
+import pyorient
+import csv
 
 # Set the connection parameters to connect to rabbit-server1 on port 5672
 # on the / virtual host using the username "guest" and password "guest"
 
+#attempts to create database
+#if succeeds: starts listening to message broker
+#if failes: catches exeption and does not start listening
+loadCheck = loadDB()
 
+if loadCheck == True:
+    
+    #database name
+    dbname = "covid_reporting_project"
+    #database login is root by default
+    login = "root"
+    #database password, set by docker param
+    password = "rootpwd"
+    #create client to connect to local orientdb docker container
+    client = pyorient.OrientDB("172.31.144.183", 2424)
+    session_id = client.connect(login, password)
+    client.db_open(dbname, login, password)
+    
+    #connect to RabbitMQ message publisher
+    username = 'student'
+    password = 'student01'
+    hostname = '128.163.202.61'
+    virtualhost = 'patient_feed'
 
-credentials = pika.PlainCredentials(username, password)
-parameters = pika.ConnectionParameters(hostname,
+    credentials = pika.PlainCredentials(username, password)
+    parameters = pika.ConnectionParameters(hostname,
                                            5672,
                                            virtualhost,
                                            credentials)
 
-connection = pika.BlockingConnection(parameters)
+    connection = pika.BlockingConnection(parameters)
 
-channel = connection.channel()
+    channel = connection.channel()
 
-exchange_name = 'patient_data'
-channel.exchange_declare(exchange=exchange_name, exchange_type='topic')
+    exchange_name = 'patient_data'
+    channel.exchange_declare(exchange=exchange_name, exchange_type='topic')
 
-result = channel.queue_declare('', exclusive=True)
-queue_name = result.method.queue
+    result = channel.queue_declare('', exclusive=True)
+    queue_name = result.method.queue
 
-binding_keys = "#"
+    binding_keys = "#"
+    
+    if not binding_keys:
+        sys.stderr.write("Usage: %s [binding_key]...\n" % sys.argv[0])
+        sys.exit(1)
 
-if not binding_keys:
-    sys.stderr.write("Usage: %s [binding_key]...\n" % sys.argv[0])
-    sys.exit(1)
+    for binding_key in binding_keys:
+        channel.queue_bind(
+                exchange=exchange_name, queue=queue_name, routing_key=binding_key)
+    
+    filepath = "hospitals.csv"
+    file =  open(filepath)
+    readFile = csv.reader(file, delimiter = ',')
+    headerInfo = next(readFile)
+    allHospitals = []
+    lvlFourHospitals = []
+    for row in readFile:
+        #append hosital IDs and zip codes
+        allHospitals.append([row[0], row[5]]) 
+        if row[16] == 'LEVEL IV':
+            lvlFourHospitals.append([row[0], row[5]])
+    
+    print("Importing zip code distances...")
+    filepath = "kyzipdistance.csv"
+    file =  open(filepath)
+    readFile = csv.reader(file, delimiter = ',')
+    headerInfo = next(readFile)
+    rowCount = 0
+    zipDict = {}
+    for row in readFile:
+        key = (row[0], row[1])
+        zipDict[key] = row[2]
+        rowCount += 1
+    
+    print('[*] Waiting for logs. To exit press CTRL+C')
 
-for binding_key in binding_keys:
-    channel.queue_bind(
-        exchange=exchange_name, queue=queue_name, routing_key=binding_key)
 
-print(' [*] Waiting for logs. To exit press CTRL+C')
-
-lastNodeFrom = -1
-
-def callback(ch, method, properties, body):
-    print(" [x] %r:%r" % (method.routing_key, body))
-    bodystr = body.decode('utf-8')
-    data = json.loads(bodystr)
-    global lastNodeFrom
-    lastNode = -2
-    count = 0
-    for payload in data:
-        mrn = payload['mrn']
-        print(mrn)
-        addNode(mrn)
-        if(lastNodeFrom == -1):
-            lastNodeFrom = mrn
-        else:
-            addEdge(lastNodeFrom,mrn)
-            lastNodeFrom = mrn
-
-
-channel.basic_consume(
-    queue=queue_name, on_message_callback=callback, auto_ack=True)
-
-channel.start_consuming()
-
+    def callback(ch, method, properties, body):
+        data = json.loads(body)
+        for patient in data:
+            #print(patient)
+            for key in patient:
+                first_name = patient["first_name"]
+                last_name = patient["last_name"]
+                mrn = patient["mrn"]
+                zip_code = patient["zip_code"]
+                patient_status_code = patient["patient_status_code"]
+                
+                #ensure that only unique patients are inserted
+                query = "SELECT COUNT(*) FROM Patient WHERE mrn = '" + mrn + "'"
+                response = client.command(query)
+                if response[0].COUNT == 0:
+                    
+                    #insert patient into database
+                    insertPatient(first_name, last_name, mrn, zip_code, patient_status_code, client)
+                    
+                    #if the patient needs any level of hospital care route to nearest hospital of any level
+                    if(patient_status_code == '3' or patient_status_code == '5'):
+                        #route to closest available hospital
+                        distances = []
+                        for i in range(len(allHospitals)):
+                            key = (zip_code, allHospitals[i][1])
+                            if key in zipDict:
+                                distances.append([allHospitals[i][0], zipDict.get(key)])
+                            else:
+                                return
+                        #find the closeset hospitals
+                        distances.sort(key = lambda x: x[1])
+                        #find databse id of closest hospital
+                        nodeIDHospital = -1
+                        for i in range(len(distances)):
+                            if nodeIDHospital == -1:
+                                query = "SELECT FROM Hospital WHERE ID = '" + str(distances[i][0]) + "'"
+                                response = client.command(query)
+                                bedCount = response[0].Beds
+                                if bedCount > 0:
+                                    nodeIDHospital = response[0]._rid 
+                        if nodeIDHospital != -1:
+                            #find database id of patient
+                            getPatientID = "SELECT FROM Patient WHERE mrn = '" + str(mrn) + "'"
+                            nodePatient = client.query(getPatientID)
+                            nodeIDPatient = str(nodePatient[0]._rid)
+                            #connect patient to hospital in database
+                            makeEdge = "CREATE EDGE FROM " + nodeIDPatient + " TO " + nodeIDHospital
+                            client.command(makeEdge)
+                            #remove a bed from the hospital
+                            updateBeds = "UPDATE " + nodeIDHospital + " INCREMENT Beds = -1"
+                            client.command(updateBeds)
+                        else:
+                            print("No hospitals with available beds for this patient")
+                            
+                    #if the patient needs emergency hospital care route them to nearest level 4 hospital
+                    elif(patient_status_code == '6'):
+                        #route to closest available level 4 hospital
+                        distances = []
+                        for i in range(len(lvlFourHospitals)):
+                            key = (zip_code, allHospitals[i][1])
+                            if key in zipDict:
+                                distances.append([allHospitals[i][0], zipDict[key]])
+                            else:
+                                return
+                        #find the closeset hospitals
+                        distances.sort(key = lambda x: x[1]) 
+                        #find databse id of closest hospital
+                        nodeIDHospital = -1
+                        for i in range(len(distances)):
+                            if nodeIDHospital == -1:
+                                query = "SELECT FROM Hospital WHERE ID = '" + str(distances[i][0]) + "'"
+                                response = client.command(query)
+                                bedCount = response[0].Beds
+                                if bedCount > 0:
+                                    nodeIDHospital = response[0]._rid
+                        if nodeIDHospital != -1:
+                            #find database id of patient
+                            getPatientID = "SELECT FROM Patient WHERE mrn = '" + str(mrn) + "'"
+                            nodePatient = client.query(getPatientID)
+                            nodeIDPatient = str(nodePatient[0]._rid)
+                            #connect patient to hospital in database
+                            makeEdge = "CREATE EDGE FROM " + nodeIDPatient + " TO " + nodeIDHospital
+                            client.command(makeEdge)
+                            #remove a bed from the hospital
+                            updateBeds = "UPDATE " + nodeIDHospital + " INCREMENT Beds = -1"
+                            client.command(updateBeds)
+                        else:
+                            print("No hospitals with available beds for this patient")
+                    
+    channel.basic_consume(
+            queue=queue_name, on_message_callback=callback, auto_ack=True)
+    
+    channel.start_consuming()
+else:
+    print("Error connecting to database")
